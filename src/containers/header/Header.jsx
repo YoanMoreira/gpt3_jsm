d254c89583a9cb29770115e330ed94b1c4807769
Moreira <yoan.moreira.ud@gmail.com>
2023-12-14 13:13:33 +0100

    header init

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import './header.css'
+import React from 'react';
+import './header.css';
 
 const Header = () => {
   return (
     <div className='gpt3__header section__padding' id='home'>
       <div className='gpt3__header-content'>
-        <h1 className='gardient__text'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+        <h1 className="gradient__text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
+        
+              
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
