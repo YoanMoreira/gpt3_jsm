598325b7bef6dc612231f4df06bedc3c837dabcf
Moreira <yoan.moreira.ud@gmail.com>
2023-12-16 16:04:08 +0100

    cta_comment

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import './cta.css'
+// Importation de la bibliothèque React et du fichier de style associé
+import React from 'react';
+import './cta.css';
 
+// Définition du composant Cta en tant que composant fonctionnel
 const Cta = () => {
   return (
+    // Conteneur principal du composant, appliquant une classe de style
     <div className='gpt3__cta'>
+      {/* Section pour le contenu du Cta */}
       <div className='gpt3__cta-content'>
+        {/* Paragraphe indiquant l'action à effectuer */}
         <p>Request Early Access to Get Started</p>
-        <h3>Register today & start exploring the endless possiblities.</h3>
+        {/* Titre du Cta */}
+        <h3>Register today & start exploring the endless possibilities.</h3>
       </div>
+      
+      {/* Section pour le bouton d'action du Cta */}
       <div className='gpt3__cta-button'>
-        <button type='button'>Get Strated</button>
+        {/* Bouton avec le texte "Get Started" */}
+        <button type='button'>Get Started</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Cta
+// Exportation du composant Cta pour pouvoir l'utiliser ailleurs dans l'application
+export default Cta;
