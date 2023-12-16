62666fefd2cddc4f970b057ddde5be8b3f743495
Moreira <yoan.moreira.ud@gmail.com>
2023-12-16 17:30:13 +0100

    footer_comment

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,22 +1,35 @@
+// Import de React depuis la bibliothèque React
 import React from 'react';
+
+// Import de l'image du logo depuis le dossier d'assets
 import gpt3Logo from '../../assets/logo.svg';
+
+// Import du fichier de style CSS pour ce composant
 import './footer.css';
 
+// Définition du composant Footer en tant que composant fonctionnel
 const Footer = () => (
+  // Conteneur principal avec une classe CSS
   <div className="gpt3__footer section__padding">
+    {/* En-tête du pied de page avec un titre stylisé */}
     <div className="gpt3__footer-heading">
-      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      <h1 className="gradient__text">Do you want to step into the future before others</h1>
     </div>
 
+    {/* Bouton de demande d'accès anticipé */}
     <div className="gpt3__footer-btn">
       <p>Request Early Access</p>
     </div>
 
+    {/* Section des liens du pied de page */}
     <div className="gpt3__footer-links">
+      {/* Logo et adresse de l'entreprise */}
       <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
         <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
       </div>
+
+      {/* Liens vers différentes sections du site */}
       <div className="gpt3__footer-links_div">
         <h4>Links</h4>
         <p>Overons</p>
@@ -24,12 +37,16 @@ const Footer = () => (
         <p>Counters</p>
         <p>Contact</p>
       </div>
+
+      {/* Liens vers les pages relatives à l'entreprise */}
       <div className="gpt3__footer-links_div">
         <h4>Company</h4>
         <p>Terms & Conditions </p>
         <p>Privacy Policy</p>
         <p>Contact</p>
       </div>
+
+      {/* Coordonnées de l'entreprise */}
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
         <p>Crechterwoord K12 182 DK Alknjkcb</p>
@@ -38,10 +55,12 @@ const Footer = () => (
       </div>
     </div>
 
+    {/* Mention de droits d'auteur */}
     <div className="gpt3__footer-copyright">
       <p>@2021 GPT-3. All rights reserved.</p>
     </div>
   </div>
 );
 
+// Export du composant Footer pour pouvoir l'utiliser ailleurs
 export default Footer;
