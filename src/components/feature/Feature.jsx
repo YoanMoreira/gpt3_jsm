0835a834d21083a40acd4d60fb1cdfa3ce19fc9b
Moreira <yoan.moreira.ud@gmail.com>
2023-12-16 14:00:58 +0100

    wgpt3/feature_comment

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,16 +1,26 @@
+// Importation de la bibliothèque React et du fichier de style associé
 import React from 'react';
 import './feature.css';
 
+// Définition du composant Feature, prenant deux propriétés (title et text)
 const Feature = ({ title, text }) => (
+  // Conteneur principal du composant, appliquant une classe de style
   <div className="gpt3__features-container__feature">
+    {/* Section du titre de la feature */}
     <div className="gpt3__features-container__feature-title">
+      {/* Barre décorative sous le titre */}
       <div />
+      {/* Titre de la feature */}
       <h1>{title}</h1>
     </div>
+
+    {/* Section du texte de la feature */}
     <div className="gpt3__features-container_feature-text">
+      {/* Paragraphe affichant le texte de la feature */}
       <p>{text}</p>
     </div>
   </div>
 );
 
+// Exportation du composant Feature pour pouvoir l'utiliser ailleurs dans l'application
 export default Feature;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,16 +1,29 @@
+// Importation de la bibliothèque React, du composant Feature et du fichier de style associé
 import React from 'react';
-import Feature from '../../components/feature/Feature';
+import Feature from '../../components/feature/Feature'; // Assurez-vous que le chemin d'importation est correct
 import './whatGPT3.css';
 
+// Définition du composant WhatGPT3 en tant que composant fonctionnel
 const WhatGPT3 = () => (
+  // Conteneur principal du composant, appliquant une classe de style et un identifiant
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+    {/* Section pour la première feature en utilisant le composant Feature */}
     <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      <Feature
+        title="What is GPT-3"
+        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+      />
     </div>
+
+    {/* Section pour le titre principal et le sous-titre */}
     <div className="gpt3__whatgpt3-heading">
+      {/* Titre principal avec un effet de dégradé de couleur */}
       <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+      {/* Sous-titre */}
       <p>Explore the Library</p>
     </div>
+
+    {/* Section pour les features suivantes, utilisant à nouveau le composant Feature */}
     <div className="gpt3__whatgpt3-container">
       <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
       <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
@@ -19,4 +32,5 @@ const WhatGPT3 = () => (
   </div>
 );
 
+// Exportation du composant WhatGPT3 pour pouvoir l'utiliser ailleurs dans l'application
 export default WhatGPT3;
